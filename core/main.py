8e5ec3368e0425481a72d9f6c22ff961d8ddfc73
prasdud <prasdudwastaken@gmail.com>
2025-10-08 14:58:37 +0530

    feat(main): simple code to capture live video stream

---
+++ core/main.py
@@ -0,0 +1,20 @@
+'''
+Sample code for reading live video stream and raw data
+'''
+
+import av
+import cv2
+import numpy as np
+
+container = av.open("/dev/video0") # Adjust the path as necessary for your video source
+
+for frame in container.decode(video=0):
+    img = frame.to_ndarray(format='bgr24')
+
+    cv2.imshow('Video', img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
+container.close()
